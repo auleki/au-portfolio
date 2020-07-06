92336d588b308c8769d0dca17669bfad8759a99b
auleki <kodagiwa@gmail.com>
2020-07-06 15:27:00 +0100

    education area completed

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -14,116 +14,130 @@ const Experience = () => {
                 <div className={s.resume}>
                     <ul>
                         <li className={s.job}>
-                            <div className={s.icon}>
-                                <i class="fas fa-briefcase"></i>
-                            </div>
-                            <div className={s.info}>
-                                <span className={s.time}>2010 - 2015</span>
-                                <h5>FRONTEND ENGINEER <span> - VIDEOHIVE</span></h5>
-                                <p>Lorem ipsum dolor sit amet consectetur, 
-                                    adipisicing elit. Eveniet vitae nobis 
-                                    fugiat repellendus vel nisi molestiae.
-                                </p>
+                            <div className={s.box}>
+                                <div className={s.icon}>
+                                    <i class="fas fa-briefcase"></i>
+                                </div>
+                                <div className={s.info}>
+                                    <span className={s.time}>2010 - 2015</span>
+                                    <h5>FRONTEND ENGINEER <span> - VIDEOHIVE</span></h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur, 
+                                        adipisicing elit. Eveniet vitae nobis 
+                                        fugiat repellendus vel nisi molestiae.
+                                    </p>
 
+                                </div>
                             </div>
                         </li>
 
                         <li className={s.job}>
-                            <div className={s.icon}>
-                                <i class="fas fa-briefcase"></i>
-                            </div>
-                            <div className={s.info}>
-                                <span className={s.time}>2010 - 2015</span>
-                                <h5>FRONTEND ENGINEER <span> - VIDEOHIVE</span></h5>
-                                <p>Lorem ipsum dolor sit amet consectetur, 
-                                    adipisicing elit. Eveniet vitae nobis 
-                                    fugiat repellendus vel nisi molestiae.
-                                </p>
+                            <div className={s.box}>
+                                <div className={s.icon}>
+                                    <i class="fas fa-briefcase"></i>
+                                </div>
+                                <div className={s.info}>
+                                    <span className={s.time}>2010 - 2015</span>
+                                    <h5>FRONTEND ENGINEER <span> - VIDEOHIVE</span></h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur, 
+                                        adipisicing elit. Eveniet vitae nobis 
+                                        fugiat repellendus vel nisi molestiae.
+                                    </p>
 
+                                </div>
                             </div>
                         </li>
 
                         <li className={s.job}>
-                            <div className={s.icon}>
-                                <i class="fas fa-briefcase"></i>
-                            </div>
-                            <div className={s.info}>
-                                <span className={s.time}>2010 - 2015</span>
-                                <h5>FRONTEND ENGINEER <span> - VIDEOHIVE</span></h5>
-                                <p>Lorem ipsum dolor sit amet consectetur, 
-                                    adipisicing elit. Eveniet vitae nobis 
-                                    fugiat repellendus vel nisi molestiae.
-                                </p>
+                            <div className={s.box}>
+                                <div className={s.icon}>
+                                    <i class="fas fa-briefcase"></i>
+                                </div>
+                                <div className={s.info}>
+                                    <span className={s.time}>2010 - 2015</span>
+                                    <h5>FRONTEND ENGINEER <span> - VIDEOHIVE</span></h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur, 
+                                        adipisicing elit. Eveniet vitae nobis 
+                                        fugiat repellendus vel nisi molestiae.
+                                    </p>
 
+                                </div>
                             </div>
                         </li>
-                        
-                        <li className={s.job}>
-                            <div className={s.icon}>
-                                <i class="fas fa-briefcase"></i>
-                            </div>
-                            <div className={s.info}>
-                                <span className={s.time}>2010 - 2015</span>
-                                <h5>FRONTEND ENGINEER <span> - VIDEOHIVE</span></h5>
-                                <p>Lorem ipsum dolor sit amet consectetur, 
-                                    adipisicing elit. Eveniet vitae nobis 
-                                    fugiat repellendus vel nisi molestiae.
-                                </p>
 
+                        <li className={s.job}>
+                            <div className={s.box}>
+                                <div className={s.icon}>
+                                    <i class="fas fa-briefcase"></i>
+                                </div>
+                                <div className={s.info}>
+                                    <span className={s.time}>2010 - 2015</span>
+                                    <h5>FRONTEND ENGINEER <span> - VIDEOHIVE</span></h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur, 
+                                        adipisicing elit. Eveniet vitae nobis 
+                                        fugiat repellendus vel nisi molestiae.
+                                    </p>
+
+                                </div>
                             </div>
                         </li>
                     </ul>
                 </div>
             {/* End of resume  */}
 
-            <div className={s.education}>
-                <ul>
+                <div className={s.education}>
+                    <ul>
 
-                    <li className={s.job}>
-                        <div className={s.icon}>
-                            <i class="fas fa-user-graduate"></i>
-                        </div>
-                        <div className={s.info}>
-                            <span className={s.time}>2010 - 2015</span>
-                            <h5>WAEC 2050 <span> - SCIENCE</span></h5>
-                            <p>Lorem ipsum dolor sit amet consectetur, 
-                                adipisicing elit. Eveniet vitae nobis 
-                                fugiat repellendus vel nisi molestiae.
-                            </p>
+                        <li className={s.job}>
+                            <div className={s.box}>
+                                <div className={s.icon}>
+                                    <i class="fas fa-user-graduate"></i>
+                                </div>
+                                <div className={s.info}>
+                                    <span className={s.time}>2010 - 2015</span>
+                                    <h5>WAEC 2050 <span> - SCIENCE</span></h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur, 
+                                        adipisicing elit. Eveniet vitae nobis 
+                                        fugiat repellendus vel nisi molestiae.
+                                    </p>
 
-                        </div>
-                    </li>
+                                </div>
+                            </div>
+                        </li>
 
-                    <li className={s.job}>
-                        <div className={s.icon}>
-                            <i class="fas fa-user-graduate"></i>
-                        </div>
-                        <div className={s.info}>
-                            <span className={s.time}>2010 - 2015</span>
-                            <h5>WAEC 2050 <span> - SCIENCE</span></h5>
-                            <p>Lorem ipsum dolor sit amet consectetur, 
-                                adipisicing elit. Eveniet vitae nobis 
-                                fugiat repellendus vel nisi molestiae.
-                            </p>
+                        <li className={s.job}>
+                            <div className={s.box}>
+                                <div className={s.icon}>
+                                    <i class="fas fa-user-graduate"></i>
+                                </div>
+                                <div className={s.info}>
+                                    <span className={s.time}>2010 - 2015</span>
+                                    <h5>WAEC 2050 <span> - SCIENCE</span></h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur, 
+                                        adipisicing elit. Eveniet vitae nobis 
+                                        fugiat repellendus vel nisi molestiae.
+                                    </p>
 
-                        </div>
-                    </li>
+                                </div>
+                            </div>
+                        </li>
 
-                    <li className={s.job}>
-                        <div className={s.icon}>
-                            <i class="fas fa-user-graduate"></i>
-                        </div>
-                        <div className={s.info}>
-                            <span className={s.time}>2010 - 2015</span>
-                            <h5>WAEC 2050 <span> - SCIENCE</span></h5>
-                            <p>Lorem ipsum dolor sit amet consectetur, 
-                                adipisicing elit. Eveniet vitae nobis 
-                                fugiat repellendus vel nisi molestiae.
-                            </p>
+                        <li className={s.job}>
+                            <div className={s.box}>
+                                <div className={s.icon}>
+                                    <i class="fas fa-user-graduate"></i>
+                                </div>
+                                <div className={s.info}>
+                                    <span className={s.time}>2010 - 2015</span>
+                                    <h5>WAEC 2050 <span> - SCIENCE</span></h5>
+                                    <p>Lorem ipsum dolor sit amet consectetur, 
+                                        adipisicing elit. Eveniet vitae nobis 
+                                        fugiat repellendus vel nisi molestiae.
+                                    </p>
 
-                        </div>
-                    </li>
-                </ul>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
             </div>
 
                 
